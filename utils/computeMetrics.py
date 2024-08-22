17b94b7070bb09eac771041c9c293783bbb8a4d5
Morales Quispe, Marcela <marcelamq@cimat.mx>
2024-08-22 11:42:58 -0600

    Fix data range for PSNR and SSIM init metrics (#13)
    * Fixed data range over all seqs at computing metrics
    * Added proper datatype for macroprops range
    * Added NSAMPLES at metrics filename for easier match
    ---------
    Signed-off-by: Morales Quispe, Marcela <marcela.morales.quispe@gmail.com>

--- utils/computeMetrics.py
+++ utils/computeMetrics.py
@@ -10,12 +10,19 @@ def psnr_mprops_seq(gt_seq_list, pred_seq_list):
     for i in range(nsamples):
         one_pred_seq =  pred_seq_list[i].cpu().numpy()
         one_gt_seq =  gt_seq_list[i].cpu().numpy()
+
+        # Calculate data ranges for each macroprop
+        rho_range = int(np.ceil(one_gt_seq[0].max() - one_gt_seq[0].min()))
+        vx_range  = int(np.ceil(one_gt_seq[1].max() - one_gt_seq[1].min()))
+        vy_range  = int(np.ceil(one_gt_seq[2].max() - one_gt_seq[2].min()))
+        unc_range = int(np.ceil(one_gt_seq[3].max() - one_gt_seq[3].min()))
+
         psnr_rho, psnr_vx, psnr_vy, psnr_unc = 0, 0, 0, 0
         for j in range(pred_len):
-            psnr_rho += psnr(one_gt_seq[0, :, :, j], one_pred_seq[0, :, :, j], data_range=one_gt_seq[0, :, :, j].max() - one_gt_seq[0, :, :, j].min())
-            psnr_vx  += psnr(one_gt_seq[1, :, :, j], one_pred_seq[1, :, :, j], data_range=one_gt_seq[1, :, :, j].max() - one_gt_seq[1, :, :, j].min())
-            psnr_vy  += psnr(one_gt_seq[2, :, :, j], one_pred_seq[2, :, :, j], data_range=one_gt_seq[2, :, :, j].max() - one_gt_seq[2, :, :, j].min())
-            psnr_unc += psnr(one_gt_seq[3, :, :, j], one_pred_seq[3, :, :, j], data_range=one_gt_seq[3, :, :, j].max() - one_gt_seq[3, :, :, j].min())
+            psnr_rho += psnr(one_gt_seq[0, :, :, j], one_pred_seq[0, :, :, j], data_range=rho_range)
+            psnr_vx  += psnr(one_gt_seq[1, :, :, j], one_pred_seq[1, :, :, j], data_range=vx_range)
+            psnr_vy  += psnr(one_gt_seq[2, :, :, j], one_pred_seq[2, :, :, j], data_range=vy_range)
+            psnr_unc += psnr(one_gt_seq[3, :, :, j], one_pred_seq[3, :, :, j], data_range=unc_range)
         # Average PSNR across frames
         mprops_nsamples_psnr[i] = (psnr_rho/pred_len, psnr_vx/pred_len, psnr_vy/pred_len, psnr_unc/pred_len)
 
@@ -31,11 +38,17 @@ def ssim_mprops_seq(gt_seq_list, pred_seq_list):
         one_gt_seq = gt_seq_list[i].cpu().numpy()
         ssim_rho, ssim_vx, ssim_vy, ssim_unc = 0, 0, 0, 0
 
+         # Calculate data ranges for each macroprop
+        rho_range = int(np.ceil(one_gt_seq[0].max() - one_gt_seq[0].min()))
+        vx_range  = int(np.ceil(one_gt_seq[1].max() - one_gt_seq[1].min()))
+        vy_range  = int(np.ceil(one_gt_seq[2].max() - one_gt_seq[2].min()))
+        unc_range = int(np.ceil(one_gt_seq[3].max() - one_gt_seq[3].min()))
+
         for j in range(pred_len):
-            ssim_rho += ssim(one_gt_seq[0, :, :, j], one_pred_seq[0, :, :, j], data_range=one_gt_seq[0, :, :, j].max() - one_gt_seq[0, :, :, j].min())
-            ssim_vx  += ssim(one_gt_seq[1, :, :, j], one_pred_seq[1, :, :, j], data_range=one_gt_seq[1, :, :, j].max() - one_gt_seq[1, :, :, j].min())
-            ssim_vy  += ssim(one_gt_seq[2, :, :, j], one_pred_seq[2, :, :, j], data_range=one_gt_seq[2, :, :, j].max() - one_gt_seq[2, :, :, j].min())
-            ssim_unc += ssim(one_gt_seq[3, :, :, j], one_pred_seq[3, :, :, j], data_range=one_gt_seq[3, :, :, j].max() - one_gt_seq[3, :, :, j].min())
+            ssim_rho += ssim(one_gt_seq[0, :, :, j], one_pred_seq[0, :, :, j], data_range=rho_range)
+            ssim_vx  += ssim(one_gt_seq[1, :, :, j], one_pred_seq[1, :, :, j], data_range=vx_range)
+            ssim_vy  += ssim(one_gt_seq[2, :, :, j], one_pred_seq[2, :, :, j], data_range=vy_range)
+            ssim_unc += ssim(one_gt_seq[3, :, :, j], one_pred_seq[3, :, :, j], data_range=unc_range)
 
         # Average SSIM across frames
         mprops_nsamples_ssim[i] = (ssim_rho/pred_len, ssim_vx/pred_len, ssim_vy/pred_len, ssim_unc/pred_len)
