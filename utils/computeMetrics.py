9f1e380dde11aa71a926a3958cd1cae0dd583f84
Morales Quispe, Marcela <marcela.morales.quispe@gmail.com>
2024-08-28 11:45:43 -0600

    Removed unc metric due to numerical issues.
    Signed-off-by: Morales Quispe, Marcela <marcela.morales.quispe@gmail.com>

--- utils/computeMetrics.py
+++ utils/computeMetrics.py
@@ -5,7 +5,7 @@ from skimage.metrics import structural_similarity as ssim
 def psnr_mprops_seq(gt_seq_list, pred_seq_list, mprops_factor):
     nsamples = len(pred_seq_list)
     _, _, _, pred_len = pred_seq_list[0].shape
-    mprops_nsamples_psnr = np.zeros((nsamples, 4))
+    mprops_nsamples_psnr = np.zeros((nsamples, 3))
 
     for i in range(nsamples):
         one_pred_seq =  pred_seq_list[i].cpu().numpy()
@@ -18,23 +18,22 @@ def psnr_mprops_seq(gt_seq_list, pred_seq_list, mprops_factor):
         rho_range = int(one_gt_seq[0].max() - one_gt_seq[0].min())
         vx_range  = int(one_gt_seq[1].max() - one_gt_seq[1].min())
         vy_range  = int(one_gt_seq[2].max() - one_gt_seq[2].min())
-        unc_range = int(one_gt_seq[3].max() - one_gt_seq[3].min())
 
         psnr_rho, psnr_vx, psnr_vy, psnr_unc = 0, 0, 0, 0
         for j in range(pred_len):
             psnr_rho += psnr(one_gt_seq[0, :, :, j], one_pred_seq[0, :, :, j], data_range=rho_range)
             psnr_vx  += psnr(one_gt_seq[1, :, :, j], one_pred_seq[1, :, :, j], data_range=vx_range)
             psnr_vy  += psnr(one_gt_seq[2, :, :, j], one_pred_seq[2, :, :, j], data_range=vy_range)
-            psnr_unc += psnr(one_gt_seq[3, :, :, j], one_pred_seq[3, :, :, j], data_range=unc_range)
-        # Average PSNR across frames
-        mprops_nsamples_psnr[i] = (psnr_rho/pred_len, psnr_vx/pred_len, psnr_vy/pred_len, psnr_unc/pred_len)
+
+        # Average PSNR across frames, except for unc channel
+        mprops_nsamples_psnr[i] = (psnr_rho/pred_len, psnr_vx/pred_len, psnr_vy/pred_len)
 
     return mprops_nsamples_psnr
 
 def ssim_mprops_seq(gt_seq_list, pred_seq_list, mprops_factor):
     nsamples = len(pred_seq_list)
     _, _, _, pred_len = pred_seq_list[0].shape
-    mprops_nsamples_ssim = np.zeros((nsamples, 4))
+    mprops_nsamples_ssim = np.zeros((nsamples, 3))
 
     for i in range(nsamples):
         one_pred_seq = pred_seq_list[i].cpu().numpy()
@@ -54,10 +53,9 @@ def ssim_mprops_seq(gt_seq_list, pred_seq_list, mprops_factor):
             ssim_rho += ssim(one_gt_seq[0, :, :, j], one_pred_seq[0, :, :, j], data_range=rho_range)
             ssim_vx  += ssim(one_gt_seq[1, :, :, j], one_pred_seq[1, :, :, j], data_range=vx_range)
             ssim_vy  += ssim(one_gt_seq[2, :, :, j], one_pred_seq[2, :, :, j], data_range=vy_range)
-            ssim_unc += ssim(one_gt_seq[3, :, :, j], one_pred_seq[3, :, :, j], data_range=unc_range)
 
-        # Average SSIM across frames
-        mprops_nsamples_ssim[i] = (ssim_rho/pred_len, ssim_vx/pred_len, ssim_vy/pred_len, ssim_unc/pred_len)
+        # Average SSIM across frames, except for unc channel
+        mprops_nsamples_ssim[i] = (ssim_rho/pred_len, ssim_vx/pred_len, ssim_vy/pred_len)
     return mprops_nsamples_ssim
 
 def lpips_mprops_seq(gt_seq_list, pred_seq_list):
