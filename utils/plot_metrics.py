bc7473f790505a69b8b96a06a4bad3d9ebc24b9d
Morales Quispe, Marcela <marcelamq@cimat.mx>
2024-11-20 13:26:42 -0600

    Added details to metrics boxplots
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- utils/plot_metrics.py
+++ utils/plot_metrics.py
@@ -1,11 +1,16 @@
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def createBoxPlot(df, title, columns_to_plot, save_path=None):
+def createBoxPlot(df, title, columns_to_plot, save_path=None, ytick_step=5):
     df[columns_to_plot].boxplot()
     plt.title(title, fontsize=16)
     plt.gca().spines[['top', 'right']].set_visible(False)
     plt.ylabel('Values')
+    # Set consistent y-axis ticks
+    y_min, y_max = df[columns_to_plot].min().min(), df[columns_to_plot].max().max()
+    yticks = np.arange(y_min // ytick_step * ytick_step, (y_max // ytick_step + 1) * ytick_step, ytick_step)
+    plt.yticks(yticks)
     # Save or show the plot
     if save_path:
         plt.savefig(save_path, bbox_inches='tight')
@@ -39,7 +44,7 @@ def createBoxPlot_bhatt(df1, df2, title, save_path=None):
         plt.show()
     plt.close()
 
-def merge_and_plot_boxplot(df_max, df, title, save_path):
+def merge_and_plot_boxplot(df_max, df, title, save_path, ytick_step):
   df_max = df_max.add_prefix('max-')
   # Concatenate both DataFrames side by side
   overall_df = pd.concat([df, df_max], axis=1)
@@ -47,4 +52,4 @@ def merge_and_plot_boxplot(df_max, df, title, save_path):
   reordered_columns = [item for pair in zip(df.columns, df_max.columns) for item in pair]
   # Reorder the DataFrame with the interleaved columns
   overall_df = overall_df[reordered_columns]
-  createBoxPlot(overall_df, title=title, columns_to_plot=overall_df.columns.tolist(), save_path=save_path)
+  createBoxPlot(overall_df, title=title, columns_to_plot=overall_df.columns.tolist(), save_path=save_path, ytick_step=ytick_step)
