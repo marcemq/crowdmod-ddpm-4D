4db1c8b6f79a701820f5e98fb130dac71a61bb3a
Morales Quispe, Marcela <marcelamq@cimat.mx>
2025-03-08 17:00:32 -0600

    Debigging save pickle
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- utils/generate_synthetic_data.py
+++ utils/generate_synthetic_data.py
@@ -26,20 +26,26 @@ def generate_synthetic_data(cfg, filenames, samples_synthetic):
     _, _, tmp_test_data, _, _, _ = dataHelper(cfg, filenames, cfg.MACROPROPS.MPROPS_COUNT, train_data_only=False, test_data_only=True)
     shuffled_indices = torch.randperm(tmp_test_data.shape[0])[:samples_synthetic]
     true_data = tmp_test_data[shuffled_indices]
-    save_pickle_file(true_data, "true_data", sdata_path)
+    logging.info(f"New synthetic_data tensor shape:{true_data.shape}")
+    try:
+        with open(sdata_path + "/true_data.pkl", 'wb') as file:
+            pickle.dump(true_data, file)
+    except MemoryError:
+        logging.info("MemoryError: Unable to pickle synthetic data due to memory issues.")
+    #save_pickle_file(true_data, "true_data", sdata_path)
 
-    synthetic_data = true_data
-    B,_,H,W,L = synthetic_data.shape
+    #synthetic_data = true_data
+    #B,_,H,W,L = synthetic_data.shape
     # Create synthetic tensor by adding a pedestrian from left to right
-    synthetic_tensor = torch.zeros((1,3,H,W,L))
-    for l in range(min(L, W)): 
-        synthetic_tensor[0, 0, 6, l, l] = 1  
-        synthetic_tensor[0, 1, 6, l, l] = 0.5
-        synthetic_tensor[0, 2, 6, l, l] = 0.0
+    #synthetic_tensor = torch.zeros((1,3,H,W,L))
+    #for l in range(min(L, W)): 
+    #    synthetic_tensor[0, 0, 6, l, l] = 1  
+    #    synthetic_tensor[0, 1, 6, l, l] = 0.5
+    #    synthetic_tensor[0, 2, 6, l, l] = 0.0
 
     # Expand synthetic_tensor to match batch size
-    synthetic_data += synthetic_tensor.expand(B, -1, -1, -1, -1)
-    save_pickle_file(synthetic_data, "synthetic_data", sdata_path)
+    #synthetic_data += synthetic_tensor.expand(B, -1, -1, -1, -1)
+    #save_pickle_file(synthetic_data, "synthetic_data", sdata_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A script to train a diffusion model for crowd macroproperties.")
