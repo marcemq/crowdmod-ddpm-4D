773bb1b30833b6953c037826bb30cf390ae374dc
Morales Quispe, Marcela <marcelamq@cimat.mx>
2025-03-08 18:20:59 -0600

    Removed to device from ndarray
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- utils/generate_synthetic_data.py
+++ utils/generate_synthetic_data.py
@@ -27,7 +27,7 @@ def generate_synthetic_data(cfg, filenames, samples_synthetic):
     # Get macroprps raw tensor
     _, _, tmp_test_data, _, _, _ = dataHelper(cfg, filenames, cfg.MACROPROPS.MPROPS_COUNT, train_data_only=False, test_data_only=True)
     shuffled_indices = torch.randperm(tmp_test_data.shape[0])[:samples_synthetic]
-    true_data = tmp_test_data[shuffled_indices].to(device)
+    true_data = tmp_test_data[shuffled_indices]
     logging.info(f"New synthetic_data tensor shape:{true_data.shape}")
     try:
         with open(sdata_path + "/true_data.pkl", 'wb') as file:
