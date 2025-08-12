6e816e4f079b8f5b0fed4ac66ef506293aeb5a94
Morales Quispe, Marcela <marcelamq@cimat.mx>
2025-08-12 13:35:29 -0600

    Updated train loss with clipped value.
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- train.py
+++ train.py
@@ -117,7 +117,7 @@ def train_convGRU(cfg, batched_train_data, batched_val_data, arch, mprops_count)
         gc.collect()
         epoch_train_loss, epoch_val_loss = train_one_epoch_convGRU(convGRU_model,batched_train_data, batched_val_data, optimizer, device, epoch=epoch, total_epochs=cfg.TRAIN.EPOCHS, teacher_forcing=cfg.MODEL.CONVGRU.TEACHER_FORCING)
         wandb.log({
-            "train_loss": epoch_train_loss,
+            "train_loss": min(epoch_train_loss, 10),
             "val_loss": epoch_val_loss
         }, step=epoch)
         # NaN handling / early stopping
