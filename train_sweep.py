3febb6d480aa53e07f8e62d88f54b992471d001d
Morales Quispe, Marcela <marcelamq@cimat.mx>
2025-08-13 22:23:26 -0600

    Added missing arch arg.
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- train_sweep.py
+++ train_sweep.py
@@ -156,7 +156,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A script to train a diffusion model for crowd macroproperties.")
     parser.add_argument('--config-yml-file', type=str, default='config/4test/ATC_ddpm.yml', help='Configuration YML file for specific dataset.')
     parser.add_argument('--configList-yml-file', type=str, default='config/4test/ATC_ddpm_datafiles.yml',help='Configuration YML macroprops list for specific dataset.')
+    parser.add_argument('--arch', type=str, default='DDPM-UNet', help='Architecture to be used, options: DDPM-UNet|ConvGRU')
     args = parser.parse_args()
-
     cfg = getYamlConfig(args.config_yml_file, args.configList_yml_file)
     train_sweep_mgmt(args, cfg)
