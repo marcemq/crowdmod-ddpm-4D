b4fc5d7b537dad813a9d623013225c5c0f9474a6
Morales Quispe, Marcela <marcelamq@cimat.mx>
2025-08-26 22:06:53 -0600

    [DEBUG] Added convGRU loss detail logs.
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- models/training.py
+++ models/training.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.cuda import amp
 from torchmetrics import MeanMetric
 from tqdm import tqdm
+import logging
 from models.diffusion.ddpm import DDPM
 from utils.loss import evaluate_loss
 
@@ -83,6 +84,7 @@ def train_one_epoch_convGRU(convGRU_model, train_data_loader, val_data_loader, o
             optimizer.step()
             train_loss_value = loss.detach().item()
             train_loss_record.update(train_loss_value)
+            logging.info(f"Training rloss:{rloss}, vloss:{vloss}")
             tq.set_postfix_str(s=f" Training Loss: {train_loss_value:.4f}")
 
         train_mean_loss = train_loss_record.compute().item()
@@ -104,6 +106,7 @@ def train_one_epoch_convGRU(convGRU_model, train_data_loader, val_data_loader, o
                 val_loss = rloss + vloss
                 val_loss_value = val_loss.detach().item()
                 val_loss_record.update(val_loss_value)
+                logging.info(f"Training rloss:{rloss}, vloss:{vloss}")
                 tq.set_postfix_str(s=f" Val Loss: {val_loss_value:.4f}")
 
         val_mean_loss = val_loss_record.compute().item()
