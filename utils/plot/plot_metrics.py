99fa27f620ae407ffcb2c834e5fb394417215d29
Morales Quispe, Marcela <marcelamq@cimat.mx>
2025-09-25 18:11:51 -0600

    Updated histograms plots fix10
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- utils/plot/plot_metrics.py
+++ utils/plot/plot_metrics.py
@@ -157,7 +157,8 @@ def plot_motion_feat_hist2D(hist_2D_list, global_count):
         # Magnitude ticks every 1
         x_step = 0.5 if len(hist_2D.mag_edges) > 10 else 1
         positions = np.arange(hist_2D.mag_edges[0], hist_2D.mag_edges[-1] + x_step, x_step)
-        labels = [f"$\\frac{{{k}}}{{2}}$" for k in range(len(positions))]
+        #labels = [f"$\\frac{{{k}}}{{2}}$" for k in range(len(positions))]
+        labels = [f"$\\displaystyle \\frac{{{k}}}{{2}}$" for k in range(len(positions))]
         plt.xticks(positions, labels, ha="center", fontsize=10)
         plt.yticks(angle_ticks, angle_tick_labels)
 
