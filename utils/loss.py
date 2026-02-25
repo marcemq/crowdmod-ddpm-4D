131624c8b2d74db87457460072fe6c9906aba498
Morales Quispe, Marcela <marcelamq@cimat.mx>
2026-02-25 10:21:31 -0600

    Removed un-used loss function used for backup
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- utils/loss.py
+++ utils/loss.py
@@ -50,31 +50,3 @@ def evaluate_loss(model, x, y, teacher_forcing, eps):
     vloss = loss_considering_density + loss_not_considering_density
 
     return rloss, vloss, loss_considering_density, loss_not_considering_density
-
-def evaluate_loss_bu(model, x, y, teacher_forcing):
-    # Forward pass
-    yhat = model(x, y, teacher_forcing=teacher_forcing)
-    # Estimated density
-    rho_hat = torch.exp(yhat[:,0:1,:,:,:]).clamp(min=1e-8, max=20)
-    # Ground truth density
-    rho_gt  = y[:,0:1,:,:,:].clamp(min=1e-8, max=20)
-    # Poisson loss
-    rloss = torch.nn.PoissonNLLLoss()
-    out_rloss = rloss(rho_hat, rho_gt)
-    out_rloss   = out_rloss.mean()
-
-    # Estimated velocity means
-    mu_hat  = yhat[:,1:3,:,:,:]
-    # Ground truth velocity means
-    mu_gt   = y[:,1:3,:,:,:]
-    # Estimated velocity variances
-    var_hat = torch.exp(yhat[:, 3:4, :, :, :]).clamp(min=1e-8, max=20)
-    var_hat = var_hat.expand_as(mu_hat)
-    # Estimated velocity variances
-    var_gt  = y[:,3:4,:,:,:].clamp(min=1e-8, max=20)
-    # Gaussian KL loss
-    vloss = torch.nn.GaussianNLLLoss()
-    out_vloss = vloss(mu_hat, mu_gt, var_hat)
-    out_vloss   = (rho_gt.repeat(1, 2, 1, 1, 1)*out_vloss).mean()
-
-    return out_rloss, out_vloss
