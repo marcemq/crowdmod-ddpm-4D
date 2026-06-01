4fffa98c7fb546844c07427bae3163afb1ec4629
Morales Quispe, Marcela <marcelamq@cimat.mx>
2026-05-31 23:24:37 -0600

    Fix correct shape at FW step
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- models/diffusion/ddpm.py
+++ models/diffusion/ddpm.py
@@ -25,9 +25,9 @@ class DDPM(ForwardSampler):
     def step(self, predicted_noise:torch.Tensor, xnoise:torch.Tensor, timestep: int):
         # Noise from normal distribution
         z  = torch.randn_like(xnoise) if timestep > 0 else torch.zeros_like(xnoise)
-        beta_t                     = self.beta[timestep].reshape(-1, 1, 1)
-        one_by_sqrt_alpha_t        = self.one_by_sqrt_alpha[timestep].reshape(-1, 1, 1)
-        sqrt_one_minus_alpha_bar_t = self.sqrt_one_minus_alpha_bar[timestep].reshape(-1, 1, 1)
+        beta_t                     = self.beta[timestep].reshape(-1, 1, 1, 1, 1)
+        one_by_sqrt_alpha_t        = self.one_by_sqrt_alpha[timestep].reshape(-1, 1, 1, 1, 1)
+        sqrt_one_minus_alpha_bar_t = self.sqrt_one_minus_alpha_bar[timestep].reshape(-1, 1, 1, 1, 1)
         # Use the formula above to sample a denoised version from the noisy one
         # equation at Algorithm 2 Sampling pseudocode
         xdenoised = (
