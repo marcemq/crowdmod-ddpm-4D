20d65451134b2c0be55492e59315e0fed32a58d6
Morales Quispe, Marcela <marcelamq@cimat.mx>
2026-08-21 11:57:13 -0600

    Fix errors - 2
    Signed-off-by: Morales Quispe, Marcela <marcelamq@cimat.mx>

--- models/diffusion/ddpm.py
+++ models/diffusion/ddpm.py
@@ -428,6 +428,7 @@ class DDPM_model:
         logging.info("===" * 20)
         logging.info(f'Computing prediction variability with {self.arch} model.')
 
+        # Plot only discting n_seqs_to_plot and its repeated predictions to see in gif comparison the variability of predictions
         setup_predictions_plot(x[:n_seqs_to_plot*n_repeats], random_past_idx[:n_seqs_to_plot*n_repeats], random_past_samples[:n_seqs_to_plot*n_repeats], random_future_samples[:n_seqs_to_plot*n_repeats], model_fullname, plotType, plotMprop, plotPast, macropropPlotter)
         logging.info(f"All sampling macroprops seqs saved in {self.output_dir}")
 
